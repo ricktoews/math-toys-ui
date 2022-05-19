f40404a8d95f0150c54532e3158322fc9ba7766f
Rick Toews <rick.toews@gmail.com>
2022-05-18 21:59:52 -0700

    Begin on popup layer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
 import Main from './Main';
 import Phi from './components/Phi';
 import PythagCList from './components/PythagCList';
@@ -10,8 +11,21 @@ import Calendar from './components/Calendar';
 
 function App() {
   
+  useEffect(() => {
+  //  console.log('App useEffect', popupRef.current);
+    
+  });
+
+  const popupRef = useRef(null);
+  const handleClick = e => {
+    popupRef.current.classList.add('hide-popup');
+    console.log(popupRef.current.classList);
+  }
+  
   return (
     <div className="App">
+            <div ref={popupRef} onClick={handleClick} className="hide-popup popup-wrapper"><div className="popup">Phi Popup</div></div>
+
       <header>
       RickToews.me
       </header>
