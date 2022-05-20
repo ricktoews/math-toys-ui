4f20bf6bc2faf40665411e3f1ad1e362a2521d20
Rick Toews <rick.toews@gmail.com>
2022-05-19 20:03:53 -0700

    Remove beginnings of popup code, since React-Bootstrap Modal popup works just fine.

--- src/App.js
+++ src/App.js
@@ -11,21 +11,8 @@ import Calendar from './components/Calendar';
 
 function App() {
   
-  useEffect(() => {
-  //  console.log('App useEffect', popupRef.current);
-    
-  });
-
-  const popupRef = useRef(null);
-  const handleClick = e => {
-    popupRef.current.classList.add('hide-popup');
-    console.log(popupRef.current.classList);
-  }
-  
   return (
     <div className="App">
-            <div ref={popupRef} onClick={handleClick} className="hide-popup popup-wrapper"><div className="popup">Phi Popup</div></div>
-
       <header>
       RickToews.me
       </header>
