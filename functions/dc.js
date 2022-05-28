5cf4a74d975356f439401eb4c2e17be8165fe32a
Rick Toews <rick.toews@gmail.com>
2022-05-27 20:46:32 -0700

    Try to fix serverless function error.

--- functions/dc.js
+++ functions/dc.js
@@ -1,9 +1,9 @@
-const dc = require('./dc/index.js');
+const denom_calc = require('./dc/index.js');
 
 exports.handler = async (event) => {
   const { path } = event;
   const denom = path.replace('/api/dc/', '');
-  let result = dc.getExpansions(denom);
+  let result = denom_calc.getExpansions(denom);
   return {
     statusCode: 200,
     body: JSON.stringify(result),
