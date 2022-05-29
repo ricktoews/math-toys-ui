973bc47e4146f090ce439cb185dfc272820f9bca
Rick Toews <rick.toews@gmail.com>
2022-05-28 21:05:44 -0700

    Still another experiment for serverless function. Seems to work.

--- functions/dcx.js
+++ functions/dcx.js
@@ -2,13 +2,12 @@ const denom_calc = require('./dc/index.js');
 
 exports.handler = async (event) => {
   const { path } = event;
-  /*
   const denom = path.replace('/api/dc/', '');
   let result = denom_calc.getExpansions(denom);
-  */
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, world", event }),
+    body: JSON.stringify(result),
   };
 
 }
