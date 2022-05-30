efaa5aabdb44f88972258a5d1f973ea572ae1afa
Rick Toews <rick.toews@gmail.com>
2022-05-30 15:11:59 -0700

    Fix decimal expansion to correctly display those that resolve.

--- src/components/denom-helper.js
+++ src/components/denom-helper.js
@@ -17,6 +17,7 @@ export function getDenominatorFactors(denom) {
 }	
 
 export function getPeriodJSX(data) {
+  console.log('getPeriodData', data);
   const { nonRepeating, firstHalf, secondHalf } = data;
   const nonRepeatingJSX = <span className="non-repeating">{nonRepeating}</span>;
   const firstHalfJSX = <span className="first-half">{firstHalf}</span>;
@@ -28,20 +29,37 @@ export function getPeriodJSX(data) {
 }
 
 export function parsedPeriod(period, beginRepeat) {
+  console.log('parsedPeriod', period, beginRepeat);
   const beginRepeatZeroBased = beginRepeat - 1;
   let nonRepeating, firstHalf, secondHalf;
   let parsed;
-  if (beginRepeatZeroBased) {
-    nonRepeating = period.substring(0, beginRepeatZeroBased);
-    period = period.substring(beginRepeatZeroBased);
-  }
-  if (period.length % 2 === 0) {
-    firstHalf = period.substring(0, period.length / 2);
-    secondHalf = period.substring(period.length / 2);
-  } else {
-    firstHalf = period;
-  }
   
+  // Decimal resolves, so no repeating portion.
+  if (beginRepeat === -1) {
+    nonRepeating = period;
+  } 
+  
+  // Decimal does not resolve, so check where repeating portion begins.
+  else {
+    // If repeating portion begins after the first digit in the expansion, extract the non-repeating part,
+    // and get the repeating part as the period.
+    if (beginRepeatZeroBased > 0) {
+      nonRepeating = period.substring(0, beginRepeatZeroBased);
+      period = period.substring(beginRepeatZeroBased);
+    }
+
+    // Now that we have the repeating period, see if it has an even number of digits.
+    // A period with an even length will have the two halves complementary.
+    if (period.length % 2 === 0) {
+      firstHalf = period.substring(0, period.length / 2);
+      secondHalf = period.substring(period.length / 2);
+    } 
+    
+    // Otherwise--period with an odd number of digits--the period is not internally complementary.
+    else {
+      firstHalf = period;
+    }
+  }
   parsed = { nonRepeating, firstHalf, secondHalf };
   return parsed;
 }
