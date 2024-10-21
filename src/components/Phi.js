de1dd20926ee4b76fdf50b6868e0e091e8ce1c3c
Rick Toews <rick.toews@gmail.com>
2024-10-21 22:07:01 +0300

    Try adding real and whole element to phi display.

--- src/components/Phi.js
+++ src/components/Phi.js
@@ -17,7 +17,7 @@ function MyVerticallyCenteredModal(props) {
   const xyPowerSuperscript = convertToSuperscript(xyPower);
   const termsExponentsSuperscript = convertToSuperscript(termsExponents.join(' + '));
   const fibonacciSum = eval(reducedFibonacci);
-  
+
   return (
     <Modal
       {...props}
@@ -32,13 +32,13 @@ function MyVerticallyCenteredModal(props) {
       </Modal.Header>
       <Modal.Body>
         <p>Numerator: ({radicalSymbol5} + 1)<sup>{power}</sup>.</p>
-        <p>Denominator: 2<sup>{power}</sup> = {2**power}. Numerator will be divided by {2**(power-1)} to yield a result with denominator 2.</p>
-        <p>Calculate numerator as (x + y)<sup>{power}</sup> = <span dangerouslySetInnerHTML={{__html:xyPowerSuperscript}} />,<br/>
-        where x = {radicalSymbol5}, y = 1.</p>
-        <p>So: <span dangerouslySetInnerHTML={{__html: termsExponentsSuperscript}} /></p>
-        <p>Compute {radicalSymbol5}<sup>n</sup>: <span dangerouslySetInnerHTML={{__html: terms.join(' + ')}} /></p>
-        <p>Fibonacci terms (<i>n</i>{radicalSymbol5}): <span dangerouslySetInnerHTML={{__html: fibonacciTerms}} /> = {fibonacciSum}</p>
-        <p>So the {power}th Fibonacci number is {fibonacciSum} / {2**(power-1)} = {fibonacciSum / 2**(power-1)}.</p>
+        <p>Denominator: 2<sup>{power}</sup> = {2 ** power}. Numerator will be divided by {2 ** (power - 1)} to yield a result with denominator 2.</p>
+        <p>Calculate numerator as (x + y)<sup>{power}</sup> = <span dangerouslySetInnerHTML={{ __html: xyPowerSuperscript }} />,<br />
+          where x = {radicalSymbol5}, y = 1.</p>
+        <p>So: <span dangerouslySetInnerHTML={{ __html: termsExponentsSuperscript }} /></p>
+        <p>Compute {radicalSymbol5}<sup>n</sup>: <span dangerouslySetInnerHTML={{ __html: terms.join(' + ') }} /></p>
+        <p>Fibonacci terms (<i>n</i>{radicalSymbol5}): <span dangerouslySetInnerHTML={{ __html: fibonacciTerms }} /> = {fibonacciSum}</p>
+        <p>So the {power}th Fibonacci number is {fibonacciSum} / {2 ** (power - 1)} = {fibonacciSum / 2 ** (power - 1)}.</p>
       </Modal.Body>
       <Modal.Footer>
         <Button className="app-btn" onClick={props.onHide}>Close</Button>
@@ -49,79 +49,83 @@ function MyVerticallyCenteredModal(props) {
 
 function Phi(props) {
   const [modalShow, setModalShow] = useState(false);
-  const [ phiData, setPhiData ] = useState([]);
-  const [ phiDissect, setPhiDissect ] = useState({});
+  const [phiData, setPhiData] = useState([]);
+  const [phiDissect, setPhiDissect] = useState({});
 
-    useEffect(() => {
-        (async () => {
-            let data = await phi(18);
-//            console.log('useEffect data', data);
-            setPhiData(data);
-        })();
-    }, [phiData.length]);
+  useEffect(() => {
+    (async () => {
+      let data = await phi(18);
+      console.log('useEffect data', data);
+      setPhiData(data);
+    })();
+  }, [phiData.length]);
 
-    async function phi(n) {
-        let data = await getPhi(n);
-        return data;
-    }
+  async function phi(n) {
+    let data = await getPhi(n);
+    return data;
+  }
 
-    const handleRowClick = e => {
-      const row = e.currentTarget;
-      const popupData = {};
-      const power = row.dataset.power;
-      popupData.power = power;
-      popupData.pascalRow = getPascalRow(power);
-      const xyPower = constructXYPower(power);
-      popupData.xyPower = xyPower;
-      const termsExponents = constructPhiPower(power);
-      popupData.termsExponents = termsExponents;
-      const terms = constructPhiPower(power, false);
-      popupData.terms = terms;
-      const fibonacciTerms = isolateFibonacciTerms(terms).sum;
-      popupData.fibonacciTerms = fibonacciTerms;
-      const reduced = reducedTerms(terms);
-      popupData.reduced = reduced;
-      const reducedFibonacci = isolateFibonacciTerms(reduced).sum
-      popupData.reducedFibonacci = reducedFibonacci;
-      const combined = combineTerms(reduced);
-      popupData.combined = combined;
-      setPhiDissect(popupData);
-      setModalShow(true);
-    }
+  const handleRowClick = e => {
+    const row = e.currentTarget;
+    const popupData = {};
+    const power = row.dataset.power;
+    popupData.power = power;
+    popupData.pascalRow = getPascalRow(power);
+    const xyPower = constructXYPower(power);
+    popupData.xyPower = xyPower;
+    const termsExponents = constructPhiPower(power);
+    popupData.termsExponents = termsExponents;
+    const terms = constructPhiPower(power, false);
+    popupData.terms = terms;
+    const fibonacciTerms = isolateFibonacciTerms(terms).sum;
+    popupData.fibonacciTerms = fibonacciTerms;
+    const reduced = reducedTerms(terms);
+    popupData.reduced = reduced;
+    const reducedFibonacci = isolateFibonacciTerms(reduced).sum
+    popupData.reducedFibonacci = reducedFibonacci;
+    const combined = combineTerms(reduced);
+    popupData.combined = combined;
+    setPhiDissect(popupData);
+    setModalShow(true);
+  }
 
-    return (<div>
-      <h1>Powers of Phi</h1>
-      <Table striped hover>
-        <thead className="sticky-table">
-          <tr>
-            <th>Fraction of Phi<sup>n</sup></th>
-            <th>Fibonacci n</th>
-            <th>Lucas n</th>
-            <th>Fibonacci n x V5</th>
-          </tr>
-        </thead>
-        <tbody>
-        
-        { phiData.map((item, key) => {
-            let f_l = item['[F, F*SQRT_5, L, L/SQRT_5]'];
-            const Fibonacci = f_l[0];
-            const Lucas = f_l[2];
+  return (<div>
+    <h1>Powers of Phi</h1>
+    <Table striped hover>
+      <thead className="sticky-table">
+        <tr>
+          <th>Fraction of Phi<sup>n</sup></th>
+          <th>Fibonacci n</th>
+          <th>Lucas n</th>
+          <th>Fibonacci n x V5</th>
+          <th>Real Term</th>
+          <th>Whole Term</th>
+        </tr>
+      </thead>
+      <tbody>
 
-            return (<tr onClick={handleRowClick} key={key} data-power={key + 1}>
-              <td><MathJax>{`\\(\\frac{${Fibonacci}\\sqrt{5} + ${Lucas}}{2}\\)`}</MathJax></td>
-              <td>{f_l[0]}</td>
-              <td>{f_l[2]}</td>
-              <td>{Number.parseFloat(f_l[1]).toFixed(2)}</td>
-            </tr>)
+        {phiData.map((item, key) => {
+          let f_l = item['[F, F*SQRT_5, L, L/SQRT_5]'];
+          const Fibonacci = f_l[0];
+          const Lucas = f_l[2];
+
+          return (<tr onClick={handleRowClick} key={key} data-power={key + 1}>
+            <td><MathJax>{`\\(\\frac{${Fibonacci}\\sqrt{5} + ${Lucas}}{2}\\)`}</MathJax></td>
+            <td>{f_l[0]}</td>
+            <td>{f_l[2]}</td>
+            <td>{Number.parseFloat(f_l[1]).toFixed(2)}</td>
+            <td>{item.real_term}</td>
+            <td>{item.whole_term}</td>
+          </tr>)
         })}
-        </tbody>
-      </Table>
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        data={phiDissect}
-      />
-    </div>)
+      </tbody>
+    </Table>
+    <MyVerticallyCenteredModal
+      show={modalShow}
+      onHide={() => setModalShow(false)}
+      data={phiDissect}
+    />
+  </div>)
 }
 
 export default Phi
