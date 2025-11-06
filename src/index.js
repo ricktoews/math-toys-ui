52dbd60b637469082ba8c37839b27214a9b02b7f
Rick Toews <rick.toews@gmail.com>
2025-11-05 20:00:43 -0800

    Remove invalid option: Grok gave me bad advice--and I was fool enough to take it.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ SessionRecorder.init({
   environment: 'production',
   apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnRlZ3JhdGlvbiI6IjY4YzMzMDMxZTE0YjViY2RlZDdlZmRmMCIsIndvcmtzcGFjZSI6IjY4YzIwYTQ4ZWM5MzU5ZDhkNWQ0MjhmOSIsInByb2plY3QiOiI2OGMyMGE0YTdiNzkzMjcyNzYzNGMyMjgiLCJ0eXBlIjoiT1RFTCIsImlhdCI6MTc1NzYyMjMyMX0.D1ZLpMFLZhTs83uoKI8cy1MlOoamZbgMKe9uzByRyrM',
   propagateTraceHeaderCorsUrls: new RegExp(`https://mathtoys.toews-api.com`, 'i'),
-  sampler: 'always_on'
 });
 
 if (SessionRecorder._isInitialized === false) {
